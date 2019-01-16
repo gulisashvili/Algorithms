a98ebdc5a9448c02b00b08c9e65912c4d6b3a986
levan gulisashvili <levan.gulisashvili@gmail.com>
2019-01-17 00:41:34 +0400

    refactored binarysearch

--- BinarySearch/index.js
+++ BinarySearch/index.js
@@ -1,6 +1,6 @@
 /* Returns either the index of the location in the array,
   or -1 if the array did not contain the targetValue */
-var doSearch = function (array, targetValue) {
+var binarySearch = function (array, targetValue) {
   var min = 0;
   var max = array.length - 1;
   var guess;
@@ -8,13 +8,10 @@ var doSearch = function (array, targetValue) {
   while (min <= max) {
     guess = (Math.floor((min + max) / 2));
 
-    if (array[guess] === targetValue) {
-      return guess
-    } else if (array[guess] < targetValue) {
-      min = guess + 1
-    } else {
-      max = guess - 1
-    }
+    if (array[guess] === targetValue) { return guess } 
+    else if (array[guess] < targetValue) { min = guess + 1 } 
+    else { max = guess - 1 }
+  
   }
 
   return -1;
